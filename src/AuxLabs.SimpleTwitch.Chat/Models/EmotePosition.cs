0f0abaeffdfcd982af16ff3fefe7086fc466fc31
Aux <justin@akitaux.com>
2023-01-19 04:34:42 -0600

    Fix emote tag parsing
    Unlike what they describe in the docs, emotes are formatted like name:0-0,1-1,2-2/name:0-0,1-1,2-2 etc...

--- src/AuxLabs.SimpleTwitch.Chat/Models/EmotePosition.cs
+++ src/AuxLabs.SimpleTwitch.Chat/Models/EmotePosition.cs
@@ -3,10 +3,51 @@
     public struct EmotePosition
     {
         public string Id { get; init; }
-        public int StartIndex { get; init; }
-        public int EndIndex { get; init; }
+        public IReadOnlyCollection<(int Start, int End)> Indices { get; init; }
 
-        public EmotePosition(string name, int start, int end)
-            => (Id, StartIndex, EndIndex) = (name, start, end);
+        public EmotePosition(string name, IReadOnlyCollection<(int, int)> indices)
+            => (Id, Indices) = (name, indices);
+
+        public static void Parse(string value, out EmotePosition emote)
+        {
+            var info = value.Split(':');
+            var id = info[0];
+            var positions = info[1].Split(',');
+
+            var indices = new List<(int, int)>();
+            foreach (var item in positions)
+            {
+                var position = item.Split('-');
+                int start = int.Parse(position[0]);
+                int end = int.Parse(position[1]);
+                indices.Add((start, end));
+            }
+
+            emote = new EmotePosition
+            {
+                Id = id,
+                Indices = indices.AsReadOnly()
+            };
+        }
+
+        public static bool TryParseMany(string value, out IReadOnlyCollection<EmotePosition> emotes)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                emotes = null;
+                return false;
+            }
+
+            var response = new List<EmotePosition>();
+            var emoteArr = value.Split('/');
+            foreach (var item in emoteArr)
+            {
+                Parse(item, out var emote);
+                response.Add(emote);
+            }
+
+            emotes = response.AsReadOnly();
+            return true;
+        }
     }
 }
