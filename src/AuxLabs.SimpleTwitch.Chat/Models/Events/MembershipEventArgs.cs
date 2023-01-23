5e411c89e9bee81be6142d25b1c10a682f04bb47
Aux <justin@akitaux.com>
2023-01-23 13:22:04 -0600

    Add join & part events

---
+++ src/AuxLabs.SimpleTwitch.Chat/Models/Events/MembershipEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace AuxLabs.SimpleTwitch.Chat.Models
+{
+    public class MembershipEventArgs
+    {
+        public string ChannelName { get; set; }
+        public string UserName { get; set; }
+
+        public MembershipEventArgs(IrcPrefix prefix, IReadOnlyCollection<string> parameters)
+        {
+            ChannelName = parameters.ElementAt(0).Trim('#');
+            UserName = prefix.Username;
+        }
+    }
+}
