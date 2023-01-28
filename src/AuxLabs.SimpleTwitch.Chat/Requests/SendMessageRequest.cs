b5a73b0d803756760ee275a7892d8851328ea289
Aux <justin@akitaux.com>
2023-01-28 01:22:33 -0600

    Add sendmessagerequest

---
+++ src/AuxLabs.SimpleTwitch.Chat/Requests/SendMessageRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AuxLabs.SimpleTwitch.Chat
+{
+    public class SendMessageRequest : IrcPayload
+    {
+        public SendMessageRequest(string channelName, string message, string replyMessageId = null)
+        {
+            Command = IrcCommand.Message;
+
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentNullException(nameof(message));
+            if (replyMessageId != null)
+                Tags = new SendMessageTags(replyMessageId);
+
+            var parameters = new[]
+            {
+                $"#{channelName}",
+                $" :{message}"
+            };
+            Parameters = parameters;
+        }
+    }
+}
