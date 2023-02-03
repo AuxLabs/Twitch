c99dea3ab3fdb502ca31106d11d9704956cca088
Aux <justin@akitaux.com>
2023-02-03 17:47:42 -0600

    Change client config naming

---
+++ src/AuxLabs.SimpleTwitch.Rest/TwitchRestApiConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace AuxLabs.SimpleTwitch.Rest
+{
+    public class TwitchRestApiConfig
+    {
+        /// <summary>  </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>  </summary>
+        public string ClientSecret { get; set; }
+
+        /// <summary> Should the client attempt to auto refresh tokens. </summary>
+        public bool ShouldAutoRefresh { get; set; } = true;
+
+        /// <summary>  </summary>
+        public IRateLimiter RateLimiter { get; set; } = null;
+    }
+}
