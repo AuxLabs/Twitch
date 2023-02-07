0f0658fd62d12d0cb9ebd84ac287df048fca10e4
Aux <justin@akitaux.com>
2023-02-07 03:34:33 -0600

    Add throw on unknown event

--- src/AuxLabs.SimpleTwitch.EventSub/TwitchEventSubConfig.cs
+++ src/AuxLabs.SimpleTwitch.EventSub/TwitchEventSubConfig.cs
@@ -4,6 +4,10 @@ namespace AuxLabs.SimpleTwitch.EventSub
 {
     public class TwitchEventSubConfig
     {
+        /// <summary> Should an exception be raised if an unhandled event is received from twitch. </summary>
+        public bool ThrowOnUnknownEvent { get; set; } = false;
+
+        /// <summary> Specify a custom serializer for eventsub json messages. </summary>
         public ISerializer<EventSubWebSocketPayload> Serializer { get; set; } = null;
     }
 }
