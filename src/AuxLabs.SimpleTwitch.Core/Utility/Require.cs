6df76fe2ad0577e1ee70fdee99985aef8da22ba2
Aux <justin@akitaux.com>
2023-02-07 23:22:31 -0600

    Add static assertions class

---
+++ src/AuxLabs.SimpleTwitch.Core/Utility/Require.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace AuxLabs.SimpleTwitch
+{
+    public static class Require
+    {
+        #region Objects
+
+        public static void NotNull(object obj, string name, string msg = null) 
+        { 
+            if (obj == null) throw new ArgumentNullException(name, msg ?? "Argument cannot be null"); 
+        }
+
+        #endregion
+        #region Strings
+
+        public static void NotEmpty(string obj, string name, string msg = null)
+        {
+            if (obj == null) throw new ArgumentException(msg ?? "Argument cannot be blank", name);
+        }
+        public static void NotNullOrEmpty(string obj, string name, string msg = null)
+        {
+            NotNull(obj, name, msg);
+            NotEmpty(obj, name, msg);
+        }
+        public static void NotNullOrWhitespace(string obj, string name, string msg = null)
+        {
+            NotNullOrEmpty(obj, name, msg);
+            if (obj.Trim().Length == 0) throw new ArgumentException(msg ?? "Argument cannot be blank", name);
+        }
+
+        #endregion
+        #region Numbers
+
+        public static void AtLeast(int obj, int value, string name, string msg = null)
+        {
+            if (obj < value) throw new ArgumentException(msg ?? $"Value must be at least {value}", name);
+        }
+        public static void GreaterThan(int obj, int value, string name, string msg = null)
+        {
+            if (obj <= value) throw new ArgumentException(msg ?? $"Value must be greater than {value}", name);
+        }
+        public static void AtMost(int obj, int value, string name, string msg = null)
+        {
+            if (obj > value) throw new ArgumentException(msg ?? $"Value must be at most {value}", name);
+        }
+        public static void LessThan(int obj, int value, string name, string msg = null)
+        {
+            if (obj >= value) throw new ArgumentException(msg ?? $"Value must be less than {value}", name);
+        }
+
+        #endregion
+        #region Identity
+
+        public static void Scopes(string[] has, string[] value)
+        {
+            if (has.Any(x => value.Contains(x))) throw new MissingScopeException(value);
+        }
+
+        #endregion
+    }
+}
