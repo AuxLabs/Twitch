f18fe9459c8658f6d6e1cea1af83c2a5d51d1a11
Aux <justin@akitaux.com>
2023-02-17 17:15:36 -0600

    Add automod moderation folder

--- src/AuxLabs.SimpleTwitch.Rest/Models/Moderation/Ban.cs
+++ src/AuxLabs.SimpleTwitch.Rest/Models/Moderation/Ban.cs
@@ -5,40 +5,24 @@ namespace AuxLabs.SimpleTwitch.Rest
 {
     public class Ban
     {
-        /// <summary> The ID of the banned user. </summary>
-        [JsonPropertyName("user_id")]
-        public string UserId { get; set; }
+        /// <summary> The broadcaster whose chat room the user was banned from chatting in. </summary>
+        [JsonPropertyName("broadcaster_id")]
+        public string BroadcasterId { get; set; }
 
-        /// <summary> The banned user’s login name. </summary>
-        [JsonPropertyName("user_login")]
-        public string UserName { get; set; }
-
-        /// <summary> The banned user’s display name. </summary>
-        [JsonPropertyName("user_name")]
-        public string UserDisplayName { get; set; }
-
-        /// <summary> The UTC date and time of when the timeout expires </summary>
-        [JsonPropertyName("expires_at")]
-        public DateTime? ExpiresAt { get; set; }
-
-        /// <summary> The UTC date and time of when the user was banned. </summary>
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        /// <summary> The reason the user was banned or put in a timeout if the moderator provided one. </summary>
-        [JsonPropertyName("reason")]
-        public string Reason { get; set; }
-
-        /// <summary> The ID of the moderator that banned the user or put them in a timeout. </summary>
+        /// <summary> The moderator that banned or put the user in the timeout. </summary>
         [JsonPropertyName("moderator_id")]
         public string ModeratorId { get; set; }
 
-        /// <summary> The moderator’s login name. </summary>
-        [JsonPropertyName("moderator_login")]
-        public string ModeratorName { get; set; }
+        /// <summary> The user that was banned or put in a timeout. </summary>
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
 
-        /// <summary> The moderator’s display name. </summary>
-        [JsonPropertyName("moderator_name")]
-        public string ModeratorDisplayName { get; set; }
+        /// <summary> The UTC date and time that the ban or timeout was placed. </summary>
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary> The UTC date and time that the timeout will end. </summary>
+        [JsonPropertyName("end_time")]
+        public DateTime? EndsAt { get; set; }
     }
 }
---
+++ src/AuxLabs.SimpleTwitch.Rest/Models/Moderation/BannedUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace AuxLabs.SimpleTwitch.Rest
+{
+    public class BannedUser
+    {
+        /// <summary> The ID of the banned user. </summary>
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        /// <summary> The banned user’s login name. </summary>
+        [JsonPropertyName("user_login")]
+        public string UserName { get; set; }
+
+        /// <summary> The banned user’s display name. </summary>
+        [JsonPropertyName("user_name")]
+        public string UserDisplayName { get; set; }
+
+        /// <summary> The UTC date and time of when the timeout expires </summary>
+        [JsonPropertyName("expires_at")]
+        public DateTime? ExpiresAt { get; set; }
+
+        /// <summary> The UTC date and time of when the user was banned. </summary>
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary> The reason the user was banned or put in a timeout if the moderator provided one. </summary>
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; }
+
+        /// <summary> The ID of the moderator that banned the user or put them in a timeout. </summary>
+        [JsonPropertyName("moderator_id")]
+        public string ModeratorId { get; set; }
+
+        /// <summary> The moderator’s login name. </summary>
+        [JsonPropertyName("moderator_login")]
+        public string ModeratorName { get; set; }
+
+        /// <summary> The moderator’s display name. </summary>
+        [JsonPropertyName("moderator_name")]
+        public string ModeratorDisplayName { get; set; }
+    }
+}
