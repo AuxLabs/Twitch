d4ba5acac43e79554bc607c30b2fed2f26c7169f
Aux <justin@akitaux.com>
2023-02-22 20:19:48 -0600

    Add IManaged contract

---
+++ src/AuxLabs.SimpleTwitch.Rest/Requests/IManaged.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AuxLabs.SimpleTwitch.Rest
+{
+    public interface IManaged : IScoped
+    {
+        void Validate(IEnumerable<string> scopes, string authedUserId);
+    }
+}
