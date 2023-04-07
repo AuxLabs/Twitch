86551811765ce1a9fbc6f22148afebb6d35518e4
Aux <justin@akitaux.com>
2023-04-07 10:41:20 -0500

    Remove unused named entity type

--- src/AuxLabs.Twitch.Chat/Entities/ChatEntity.cs
+++ src/AuxLabs.Twitch.Chat/Entities/ChatEntity.cs
@@ -1,9 +1,9 @@
-﻿using AuxLabs.Twitch;
-using System;
+﻿using System;
 
 namespace AuxLabs.Twitch.Chat.Entities
 {
-    public abstract class ChatEntity<T> : IEquatable<ChatEntity<T>>, IEntity<T>
+    public abstract class ChatEntity<T> 
+        : IEquatable<ChatEntity<T>>, IEntity<T>
     {
         /// <summary> An ID that uniquely identifies the entity. </summary>
         public T Id { get; }
@@ -20,22 +20,5 @@ namespace AuxLabs.Twitch.Chat.Entities
             => Equals(obj as ChatEntity<T>);
         public override int GetHashCode()
             => HashCode.Combine(Id);
-    }
-
-    public abstract class ChatNamedEntity : IEquatable<ChatNamedEntity>
-    {
-        /// <summary> A name that uniquely identifies the entity. </summary>
-        public string Name { get; }
-        internal TwitchChatClient Twitch { get; }
-
-        internal ChatNamedEntity(TwitchChatClient twitch, string name)
-            => (Twitch, Name) = (twitch, name);
-
-        public bool Equals(ChatNamedEntity other)
-            => Name.Equals(other.Name);
-        public override bool Equals(object obj)
-            => Equals(obj as ChatNamedEntity);
-        public override int GetHashCode()
-            => HashCode.Combine(Name);
     }
 }
