ef24d242cf383ad0b2b48f0b0d3e8c10384f691b
Aux <justin@akitaux.com>
2023-04-07 10:36:20 -0500

    Add default max per page constant

--- src/abstractions/AuxLabs.Twitch.Core/CDN.cs
+++ src/abstractions/AuxLabs.Twitch.Core/CDN.cs
@@ -2,15 +2,13 @@
 {
     public static class CDN
     {
-        public const string EmoteImageUrl = "https://static-cdn.jtvnw.net/emoticons/v2/{0}/{1}/{2}/{3}";
-
         public static string GetEmoteImageUrl(string emoteId, EmoteFormat format = EmoteFormat.Static, EmoteTheme theme = EmoteTheme.Dark, EmoteScale scale = EmoteScale.Small)
         {
             var formatValue = format.GetStringValue();
             var themeValue = theme.GetStringValue();
             var scaleValue = scale.GetStringValue();
 
-            return string.Format(EmoteImageUrl, emoteId, formatValue, themeValue, scaleValue);
+            return string.Format(TwitchConstants.EmoteImageUrl, emoteId, formatValue, themeValue, scaleValue);
         }
     }
 }
